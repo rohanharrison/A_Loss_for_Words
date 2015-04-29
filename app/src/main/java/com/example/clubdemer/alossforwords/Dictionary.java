627a0bfeeac8b03df5256bbd3cced0e431ac27ec
zjones <zjones@hartford.edu>
2015-04-29 11:34:00 -0400

    Dictionary is a go

--- app/src/main/java/com/example/clubdemer/alossforwords/Dictionary.java
+++ app/src/main/java/com/example/clubdemer/alossforwords/Dictionary.java
@@ -1,7 +1,47 @@
 package com.example.clubdemer.alossforwords;
+import android.content.res.AssetManager;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  * Created by clubdemer on 4/6/15.
  */
 public class Dictionary {
+
+    ArrayList<String> dictionary = new ArrayList();
+    ArrayList<String> sublist = new ArrayList();
+
+     Dictionary (Scanner scan){
+         while (scan.hasNext()) {
+             dictionary.add(scan.next());
+         }
+    }
+
+
+
+    public ArrayList<String> requestList(String word){
+        String next;
+
+        for (int i = 0; i < dictionary.size(); i++){
+            next = dictionary.get(i);
+            if (next.contains(word)){
+                sublist.add(next);
+                Log.d("test", next);
+            }
+
+
+        }
+
+        return sublist;
+    }
+
+
+
+
+
 }
